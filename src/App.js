be28547d154266184433ebd3a533a438f57a86f9
shy <gerretkubota@gmail.com>
2022-10-27 19:21:54 -0700

    Testing api to retrieve number of hits

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import Counter from './Counter';
+
+const KEY = '1ccb732e-b55a-4404-ad3f-0f99c02fe44e';
 
 function App() {
   return (
@@ -6,6 +9,7 @@ function App() {
       <header>
         <h1>Project Purple Cow</h1>
       </header>
+      <Counter apiKey={KEY} />
     </div>
   );
 }
---
+++ src/Counter/Counter.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+const URL = 'https://api.countapi.xyz';
+
+const Counter = ({ apiKey }) => {
+  const [numberOfHits, setNumberOfHits] = useState(0);
+  const [error, setError] = useState({ status: false, message: '' });
+  const [loading, setLoading] = useState(false);
+
+  const getHits = async () => {
+    try {
+      setLoading(true);
+
+      if (error.status) {
+        setError({ status: false, message: '' });
+      }
+
+      const response = await fetch(`${URL}/get/${apiKey}`);
+      const result = await response.json();
+
+      setNumberOfHits(result.value);
+      setLoading(false);
+    } catch (e) {
+      console.error(e);
+      setError({ status: true, message: 'Could not retrieve hits.' });
+    }
+  };
+
+  useEffect(() => {
+    if (apiKey) {
+      getHits();
+    } else {
+      setError({ status: true, message: 'Please provide an api key.' });
+    }
+  }, []);
+
+  return (
+    <div>
+      {!loading && <p>Current number of hits: {numberOfHits}</p>}
+      {loading && <p>Loading...</p>}
+      {error.status && <p>{error.message}</p>}
+    </div>
+  );
+};
+
+Counter.propTypes = {
+  apiKey: PropTypes.string,
+};
+
+export default Counter;
